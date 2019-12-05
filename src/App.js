547ed6bd211bd8c145c4107b4567e9ce14058efb
nkannian <nkannian@purdue.edu>
2019-12-05 18:26:42 -0500

    dynamic background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import {router, route} from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import ParticlesBg from 'particles-bg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <ParticlesBg num={10} type="circle" bg={true} />
     </div>
   );
 }
